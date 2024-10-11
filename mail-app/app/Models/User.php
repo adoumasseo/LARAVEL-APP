d12e783de5ec4a0425b90cf039724fd12363adc8
adoumasseo <adoumasseo@gmail.com>
2024-10-11 14:40:35 +0100

    add relationships between the twi tables and PHPunit tests files nothing inside them

--- mail-app/app/Models/User.php
+++ mail-app/app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Message;
 
 class User extends Authenticatable
 {
@@ -33,7 +35,10 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
 }
--- mail-app/database/migrations/2024_10_11_084836_create_messages_table.php
+++ mail-app/database/migrations/2024_10_11_084836_create_messages_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->string('sender');
+            $table->json('recipients');
+            $table->text('content');
+            $table->json('files')->nullable();
             $table->timestamps();
         });
     }
